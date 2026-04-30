ece7d24ed837ee9901127ff7a18892113129afb4
Tresford Chipili <ct23019012@biust.ac.bw>
2026-04-30 11:12:56 +0200

    Fix all import extensions

--- Tresify-task-manager-frontend/src/App.jsx
+++ Tresify-task-manager-frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import List from './pages/List';
-import Details from './pages/Details';
-import AddTask from './pages/AddTask';
-import Timetable from './pages/Timetable';
-import Gym from './pages/Gym';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Navbar from './components/Navbar.jsx';
+import Home from './pages/Home.jsx';
+import List from './pages/List.jsx';
+import Details from './pages/Details.jsx';
+import AddTask from './pages/AddTask.jsx';
+import Timetable from './pages/Timetable.jsx';
+import Gym from './pages/Gym.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
 
 const isLoggedIn = () => !!localStorage.getItem('token');
 
@@ -19,18 +19,27 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public Routes - no navbar */}
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        {/* Private Routes - with navbar */}
-        <Route path="/" element={<PrivateRoute><><Navbar /><Navigate to="/home" /></></PrivateRoute>} />
-        <Route path="/home" element={<PrivateRoute><><Navbar /><Home /></></PrivateRoute>} />
-        <Route path="/list" element={<PrivateRoute><><Navbar /><List /></></PrivateRoute>} />
-        <Route path="/details/:id" element={<PrivateRoute><><Navbar /><Details /></></PrivateRoute>} />
-        <Route path="/add" element={<PrivateRoute><><Navbar /><AddTask /></></PrivateRoute>} />
-        <Route path="/timetable" element={<PrivateRoute><><Navbar /><Timetable /></></PrivateRoute>} />
-        <Route path="/gym" element={<PrivateRoute><><Navbar /><Gym /></></PrivateRoute>} />
+        <Route path="/home" element={
+          <PrivateRoute><Navbar /><Home /></PrivateRoute>
+        } />
+        <Route path="/list" element={
+          <PrivateRoute><Navbar /><List /></PrivateRoute>
+        } />
+        <Route path="/details/:id" element={
+          <PrivateRoute><Navbar /><Details /></PrivateRoute>
+        } />
+        <Route path="/add" element={
+          <PrivateRoute><Navbar /><AddTask /></PrivateRoute>
+        } />
+        <Route path="/timetable" element={
+          <PrivateRoute><Navbar /><Timetable /></PrivateRoute>
+        } />
+        <Route path="/gym" element={
+          <PrivateRoute><Navbar /><Gym /></PrivateRoute>
+        } />
       </Routes>
     </BrowserRouter>
   );
